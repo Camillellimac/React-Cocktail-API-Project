ce9f990e321587a4a66073936541ec981ce963ac
Camille Boujasson <camilleb.dev@outlook.fr>
2023-11-24 16:33:38 +0100

    use effect

--- src/pages/CocktailRandomPage.jsx
+++ src/pages/CocktailRandomPage.jsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CocktailRandomPage () {
 
     const [cocktailRandom, setCocktailRandom] = useState(null)
     
-    if (!cocktailRandom) { // si cocktailRandom est vide, le fetch se lance
+    useEffect(() => { // hook permettant d'executer le code qu'a certains mmoments 
+
     (async () => { // la fonction s'appelle toute seule
         const cocktailRandomApi = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php") // requête envoyée à l'API
         const cocktailRandomJs = await cocktailRandomApi.json() // on récupère la date en json
         setCocktailRandom(cocktailRandomJs.drinks[0]) // on accède à la data .drinks et on demande index 0 puisqu'un seul élément random sera retourné
     })(); 
-    }
+    }, []); // le tableau vide signifie qu'on veut executer le code une seule fois seulement après le chargement du composant
 
     return (
 
         <main> 
             {cocktailRandom ? // si cocktailRandom a été récupéré 
-            <h3>{cocktailRandom.strDrink}</h3> // afficher la propriété .strDrink 
+            <>
+            <h3>{cocktailRandom.strDrink}</h3> {/* afficher la propriété .strDrink  */} 
+            <img src={cocktailRandom.strDrinkThumb} alt={cocktailRandom.strDrink} />
+            </>
             : 
             <p>Loading</p>} {/* sinon afficher cette balise */}
         </main>
